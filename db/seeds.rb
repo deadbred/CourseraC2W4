ac541753d7eb72e3f1166f97b595306a72b81956
bradzhang1128@gmail.com <deadbred3@gmail.com>
2017-04-16 21:32:36 -0400

    initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+today = Date.today
+two_days_ago = Date.today - 2.days
+three_days_ago = Date.today - 3.days
+dates = [today, two_days_ago, three_days_ago]
+
 User.destroy_all
-Profile.destroy_all
 TodoList.destroy_all
-TodoItem.destroy_all
 
-fiorina = User.create! username: "Fiorina", password_digest: "as7905"
-fiorina.create_profile! first_name: "Carly", last_name: "Fiorina", birth_year: 1954, gender: "female"
+100.times { |index| TodoList.create! list_name: "List #{index}", list_due_date: dates.sample }
 
-trump = User.create! username: "Trump", password_digest: "iouerw"
-trump.create_profile! first_name: "Donald", last_name: "Trump", birth_year: 1946, gender: "male"
+TodoList.all.each do |list|
+  list.todo_items.create! [
+    { title: "Task 1", due_date: dates.sample, description: "very important task TEST", completed: false },
+    { title: "Task 2", due_date: dates.sample, description: "do something else TEST", completed: true},
+    { title: "Task 3", due_date: dates.sample, description: "learn Action Pack TEST", completed: true}
+  ]
+end
 
-carson = User.create! username: "Carson", password_digest: "k2h38a"
-carson.create_profile! first_name: "Ben", last_name: "Carson", birth_year: 1951, gender: "male"
+users = User.create! [
+  { username: "jim", password: "abc123" },
+  { username: "rich", password: "123abc" }
+]
 
-clinton = User.create! username: "Clinton", password_digest: "pa9g82"
-clinton.create_profile! first_name: "Hillary", last_name: "Clinton", birth_year: 1947, gender: "female"
-
-User.all.each_with_index do |user, i|
-	todoList = user.todo_lists.create! list_name: "list #{i}", list_due_date: Date.today+1.year
-	(1..5).each do |i|
-		todoList.todo_items.create! due_date: Date.today+1.year, title: "title #{i}", description: "This is task #{i}"
-	end
+TodoList.all.each do |list|
+  list.user = users.sample
+  list.save!
 end

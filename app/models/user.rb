92dfe59e8e3b1bbcb9c92da8e0b21d3dc62f7a48
bradzhang1128@gmail.com <deadbred3@gmail.com>
2017-04-16 23:00:00 -0400

    nested todoitems to todolists

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
 
 	validates :username, presence: true
 
+	has_secure_password
+
 	def get_completed_count
 		User.find(self.id).todo_items.where(completed: true).count
 	end
